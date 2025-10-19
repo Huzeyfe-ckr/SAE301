73fded6fe0a229ed8175cc2594a2422439ad9252
Macbook air M4 de Huzeyfe <macbookairm4dehuzeyfe@MacBook-Air-de-Huzeyfe.local>
2025-10-19 23:53:50 +0200

    us3 api réaliser manque plus que le client

--- client/src/ui/detail/index.js
+++ client/src/ui/detail/index.js
@@ -3,6 +3,10 @@ import template from "./template.html?raw";
 
 let DetailView = {
   html: function (data) {
+    let image = data.images[0];
+    // template = template.replace("{{image}}", image);
+    // console.log(image);
+    // console.log(template);
     return genericRenderer(template, data);
   },
 
