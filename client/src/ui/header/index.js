eb0c987f9345e6d457349d020236305d71df7dbc
Huzeyfe Cakir <huzeyfe.cakir@etu.unilim.fr>
2025-10-20 15:54:47 +0200

    us3 fin

--- client/src/ui/header/index.js
+++ client/src/ui/header/index.js
@@ -16,21 +16,6 @@ let HeaderView = {
     if (menuBtn && mobileMenu) {
       menuBtn.addEventListener('click', () => {
         mobileMenu.classList.toggle('hidden');
-
-        // Animation des barres du burger
-        const spans = menuBtn.querySelectorAll('span');
-        menuBtn.classList.toggle('open');
-
-        if (menuBtn.classList.contains('open')) {
-          spans[0].style.transform = 'rotate(45deg) translateY(10px)';
-          spans[1].style.opacity = '0';
-          spans[2].style.transform = 'rotate(-45deg) translateY(-10px)';
-        } else {
-          spans.forEach(span => {
-            span.style.transform = '';
-            span.style.opacity = '1';
-          });
-        }
       });
     }
 
