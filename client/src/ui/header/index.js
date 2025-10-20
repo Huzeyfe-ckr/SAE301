164c359c386360ecd98f842357053670c2f2969c
Huzeyfe Cakir <huzeyfe.cakir@etu.unilim.fr>
2025-10-20 11:30:55 +0200

    us3 fini debut menu burger

--- client/src/ui/header/index.js
+++ client/src/ui/header/index.js
@@ -1,19 +1,40 @@
 import { htmlToFragment } from "../../lib/utils.js";
 import template from "./template.html?raw";
 
-// HeaderView est un composant statique
-// on ne fait que charger le template HTML
-// en donnant la possibilité de l'avoir sous forme html ou bien de dom
-
-
-
 let HeaderView = {
   html: function () {
     return template;
   },
 
   dom: function () {
-    return htmlToFragment(template);
+    const fragment = htmlToFragment(template);
+
+    // Sélection des éléments
+    const menuBtn = fragment.querySelector('#menu-btn');
+    const mobileMenu = fragment.querySelector('#mobile-menu');
+
+    if (menuBtn && mobileMenu) {
+      menuBtn.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+
+        // Animation des barres du burger
+        const spans = menuBtn.querySelectorAll('span');
+        menuBtn.classList.toggle('open');
+
+        if (menuBtn.classList.contains('open')) {
+          spans[0].style.transform = 'rotate(45deg) translateY(10px)';
+          spans[1].style.opacity = '0';
+          spans[2].style.transform = 'rotate(-45deg) translateY(-10px)';
+        } else {
+          spans.forEach(span => {
+            span.style.transform = '';
+            span.style.opacity = '1';
+          });
+        }
+      });
+    }
+
+    return fragment;
   }
 };
 
